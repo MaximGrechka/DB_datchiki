9a3adc79fd5077503d04b5cfb5b0bad98eb84e26
Gamer <gamer@TargonShip.CRAZYLABS>
2019-07-06 14:49:46 +0000

    Дабавлен constraint в indications_info

--- source/indications_info.sql
+++ source/indications_info.sql
@@ -1,8 +1,13 @@
 DROP TABLE IF EXISTS measure_info;
 CREATE TABLE measure_info
 (
+    id_indication bigserial PRIMARY KEY,
     id_sensor integer NOT NULL,
     received_timestamp timestamp NOT NULL,
     measure double NOT NULL,
     sos boolean DEFAULT false
+
+    CONSTRAINT MI_id_sensor_fkey FOREIGN KEY (id_sensor)
+        REFERENCES sensor_instances_info (id_sensor) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
 );
