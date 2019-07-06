4d67f74d1ab8de4d91ffe18084a0ecd41317d82b
Artem <artem@SensorsDataServer.TERMALSENSORS>
2019-07-06 12:43:20 +0300

    Добавлены скрипты для создания двух таблиц

---
+++ source/geo_info.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS geo_info;
+CREATE TABLE geo_info
+(
+    id_pos serial primary key,
+    place varchar
+);
---
+++ source/indications_info.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS indication_info; 
+CREATE TABLE indication_info
+(
+indication double,
+sos boolean, 
+id_sensor integer,
+);
